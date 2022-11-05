3a218dbeb6abea527e39cb3ff3a1540288e26eef
Aline Otília <aos_santos@hotmail.com>
2022-11-04 22:06:44 -0300

    adicionando módulo de formulário

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  formLogin = this.formBuilder.group({
+    email:['',Validators.compose([Validators.required, Validators.email])],
+    senha:['',Validators.compose([Validators.required, Validators.minLength(6)])],
+  });
+
+  mensagemErro = {
+    email: [{tipo:'required', aviso:'Digite um e-mail!'}, {tipo:'email', aviso:'Tem que ser um e-mail!'}],
+    senha: [{tipo:'required', aviso:'Digite uma senha!'}, {tipo:'minLength', aviso:'No mínimo 6 dígitos!'}],
+  };
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
   }
