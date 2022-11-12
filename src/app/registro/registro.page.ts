993b9b0254c2564cf981871cf44c7ffd169167f4
vi-sabio <vinicios_sabio@hotmail.com>
2022-11-11 22:20:25 -0300

    criado Registro

--- src/app/registro/registro.page.ts
+++ src/app/registro/registro.page.ts
@@ -7,19 +7,13 @@ import { FormBuilder, Validators } from '@angular/forms';
   styleUrls: ['./registro.page.scss'],
 })
 export class RegistroPage implements OnInit {
-  formLogin = this.formBuilder.group({
+  formregistro = this.formBuilder.group({
     nome: [
       '',
       Validators.compose([Validators.required, Validators.minLength(3)]),
     ],
-    email: [
-      '',
-      Validators.compose([
-        Validators.required,
-        Validators.email,
-        Validators.minLength(6),
-      ]),
-    ],
+    email: ['', 
+      Validators.compose([Validators.required, Validators.email,Validators.minLength(6)])],
     cpf: [
       '',
       Validators.compose([Validators.required, Validators.maxLength(11),Validators.minLength(11)]),
@@ -33,7 +27,52 @@ export class RegistroPage implements OnInit {
       Validators.compose([Validators.required, Validators.minLength(8)]),
     ],
   });
+
+  mensagemErro = {
+    email: [
+      { tipo: 'required', aviso: 'Digite um e-mail!' },
+      { tipo: 'email', aviso: 'Tem que ser um e-mail!' },
+    ],
+    senha: [
+      { tipo: 'required', aviso: 'Digite uma senha!' },
+      { tipo: 'minlength', aviso: 'No mínimo 8 dígitos!' },
+    ],
+    nome: [
+      { tipo: 'required', aviso: 'Digite uma senha!' },
+      { tipo: 'minlength', aviso: 'No mínimo 3 dígitos!' },
+    ],
+    cpf: [
+      { tipo: 'required', aviso: 'Digite uma senha!' },
+      { tipo: 'minlength', aviso: 'No mínimo e máximo 11 dígitos!' },
+    ],
+    confirmeSenha: [
+      { tipo: 'required', aviso: 'Digite uma senha!' },
+      { tipo: 'minlength', aviso: 'No mínimo 8 dígitos!' },
+    ],
+  };
+
   constructor(private formBuilder: FormBuilder) {}
+  
+  get email() {
+    return this.formregistro.get('email');
+  }
+
+  get senha() {
+    return this.formregistro.get('senha');
+  }
+
+  get nome() {
+    return this.formregistro.get('nome');
+  }
+
+  get cpf() {
+    return this.formregistro.get('cpf');
+  }
+
+  get confirmaSenha() {
+    return this.formregistro.get('confirmaSenha');
+  }
+
 
   ngOnInit() {}
 }
