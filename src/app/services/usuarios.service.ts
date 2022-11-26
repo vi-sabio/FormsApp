df94051649c5e19f54df8eb2572cb2b173fe9085
vi-sabio <vinicios_sabio@hotmail.com>
2022-11-25 22:25:26 -0300

    prova

--- src/app/services/usuarios.service.ts
+++ src/app/services/usuarios.service.ts
@@ -12,53 +12,53 @@ export class UsuariosService {
   constructor(private storageService: StorageService) { }
 
   async login(email: string, senha: string) {
-    this.buscarTodos();
+    await this.buscarTodos();
     let usuario: Usuario;
     this.listaUsuarios.filter((item => {
       if (item.email.toLocaleLowerCase() == email.toLocaleLowerCase()) {
         usuario = item;
       }
-    });
+    }));
 
     if (usuario?.senha === senha) {
       return usuario;
     }
     return null;
   }
-  
+
 
   // crud - create, read, update, delete
   // recebe um usuario para salvar
-  async salvar(usuario: Usuario){
-    this.buscarTodos();
+  async salvar(usuario: Usuario) {
+    await this.buscarTodos();
     this.listaUsuarios[usuario.id] = usuario;
-    this.storageService.set('usuario', this.listaUsuarios);
-  
+    this.storageService.set('usuarios', this.listaUsuarios);
+
   }
-  async buscarUm(id: number){
-    this.buscarTodos();
+  async buscarUm(id: number) {
+    await this.buscarTodos();
     return this.listaUsuarios[id];
   }
-  async buscarTodos(){
+  async buscarTodos() {
     this.listaUsuarios = (await this.storageService.get('usuarios')) as unknown as Usuario[];
-    if(!this.listaUsuarios) {
-      return [];   // return mata 
+    if (!this.listaUsuarios) {
+      this.listaUsuarios = [];
     }
     return this.listaUsuarios;
   }
-  async deletar(id: number){
-    this.buscarTodos();
+  async deletar(id: number) {
+    await this.buscarTodos();
     this.listaUsuarios.splice(id, 1);           // splice pega a posição e deleta apatir de uma posição e quantidade 
     this.storageService.set('usuarios', this.listaUsuarios);
   }
 
-  async salvarID(id: number){
+  async salvarID(id: number) {
     await this.storageService.set('idUsuario', id);
   }
-  async buscarID(){
+  async buscarID() {
     const id = await this.storageService.get('idUsuario');
 
-    if(!id) {
+    if (!id) {
       return 0;
     }
 
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Usuario } from '../models/Usuario.model';
+import { UsuariosService } from '../services/usuarios.service';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit{
 
-  constructor() {}
+  listaUsuarios: Usuario[] = [];
+
+
+  constructor(private userService: UsuariosService) {}
+
+  async buscarUsuarios(){
+    this.listaUsuarios = await this.userService.buscarTodos();
+  }
+
+  ngOnInit(): void {
+    
+  }
+
+  ionViewWillEnter(): void{
+    this.buscarUsuarios();
+  }
 
 }
